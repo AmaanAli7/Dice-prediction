09dfcdbfe3ab6ce6af18a83bf8fc0ab6e28cdc75
amaan ali <amaanalikhaan58833@gmail.com>
2026-02-17 03:16:25 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
  import {useState} from "react";
  import StartGame from "./components/StartGame";
 import GamePlay from "./components/GamePlay";
+
+
   function App() {
     const [isGameStarted, setIsGameStarted] = useState(false);
     const toggleGamePlay=()=> {
